df81e37e518486473353c748594d43dfbbf7c940
Rezkiyah Rahmadani <rezkyahrahmadano@gmail.com>
2024-09-12 11:08:29 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// ini javascript
+
+// Luas Segitiga
+function hitungLuasSegitiga() {
+    let alas = parseFloat(document.getElementById("alas_segitiga").value);
+    let tinggi = parseFloat(document.getElementById("tinggi_segitiga").value);
+    if (isNaN(alas) || isNaN(tinggi)) {
+      alert("Masukkan Angka Yang Valid");
+      return;
+    }
+    let luas = 0.5 * alas * tinggi;
+  
+    document.getElementById("luas_segitiga").value = luas;
+  }
